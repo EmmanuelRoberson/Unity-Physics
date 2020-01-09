492fabd060c81a74a3f405b40fa31a58df2fad0f
Emmanuel Roberson <emmanuel.roberson3@gmail.com>
2020-01-09 09:08:42 -0600

    Progress commit, Neede to do player controller

---
+++ Assets/Scripts/CannonBall.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour
+{
+    //custom
+   public GameObject target;
+    
+    public float forceOnFire = 300;
+
+    private bool fire = false;
+    private bool canFire = true;
+
+    private Rigidbody rigidbody = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //custom
+        transform.LookAt(target.transform);
+    }
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown && canFire)
+        {
+            rigidbody.isKinematic = false;
+            rigidbody.AddForce(transform.forward * forceOnFire);
+            canFire = false;
+        }
+    }
+}
